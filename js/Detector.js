faaa04d5dcfa918276046c34f8cd92d2d78dbbf3
Jmenzies722 <Jmenzies722@gmail.com>
2023-08-31 17:37:22 -0400

    check if canvas is supported

--- js/Detector.js
+++ js/Detector.js
@@ -1,1 +1,17 @@
 // Author: Josh Menzies -  https://jmenzies722.github.io/Menzies-Portfolio/port.html
+var Detector = {
+
+    // Check if canvas is supported
+    canvas: !!window.CanvasRenderingContext2D,
+
+    // Check if WebGL is supported
+    webgl: (function () {
+        try {
+            return !!(
+                window.WebGLRenderingContext &&
+                !!document.createElement('canvas').getContext('experimental-webgl')
+            );
+        } catch (e) {
+            return false;
+        }
+    })(),
